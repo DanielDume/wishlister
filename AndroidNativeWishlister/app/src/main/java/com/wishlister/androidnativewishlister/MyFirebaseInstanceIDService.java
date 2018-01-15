e92ab5fe3c3c892c80d7c6eff7cbd0325a5e63d8
Daniel <dume.daniel@ymail.com>
2018-01-15 23:28:11 +0200

    minor bugfix

---
+++ AndroidNativeWishlister/app/src/main/java/com/wishlister/androidnativewishlister/MyFirebaseInstanceIDService.java
@@ -0,0 +1,11 @@
+package com.wishlister.androidnativewishlister;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Daniel on 1/15/2018.
+ */
+
+public class MyFirebaseInstanceIDService{
+    private static final String TAG = "MyFirebaseInsIDService";
+}
